a2e3a5fd85b0ebedbb29ecf284f115e89688589e
A1llex <alexg.fernandeza@ciencias.unam.mx>
2019-09-17 12:52:11 -0500

    agregadas

---
+++ Practica02/interp.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void){
+    //string entrada="";
+    int entrada;
+    int salida =0;
+    cin>>entrada;
+
+    vector<int> V;
+    // for (size_t i = 0; i < entrada.length; i++)
+    // {
+    //     if(entrada[i]=='*')
+    //     {
+    //         if (i-1 < 0)
+    //         {
+    //             salida = 0 ;
+    //         }else{
+    //             salida+= entrada[i-1] * entrada[i+1];
+    //         }
+            
+    //     }else{
+
+    //     }
+    // }
+    cout<<entrada;
+    return 0;
+}
